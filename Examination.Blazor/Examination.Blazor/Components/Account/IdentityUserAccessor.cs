557a75f6188815e7a12c63c468c2a64c1fca304a
785732 <785732@WX-B4450669F8C9>
2024-07-30 14:38:32 +0200

    Add project files.

---
+++ Examination.Blazor/Examination.Blazor/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using Examination.Blazor.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace Examination.Blazor.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
